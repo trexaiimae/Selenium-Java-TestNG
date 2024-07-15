efcb27bbaee6ceef0115770533be2c025eccda5f
trexaiimae <trexaiihurtado@gmail.com>
2024-07-15 11:15:05 +0800

    First commit

---
+++ src/DynamicDropdowns.java
@@ -0,0 +1,40 @@
+// testing with dropdown which is dependent to other dropdown. Example from and to
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class DynamicDropdowns {
+
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Trexie\\Documents\\Installers\\Work\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+	
+		
+		//maximize browser size
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		
+		
+		
+	
+		
+		////a[@text='Chennai (MAA)'] xpath for chennai  (//a[@text='Chennai (MAA)'])[2]
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("(//a[@text='Bengaluru (BLR)'])[1]")).click();
+		Thread.sleep(2000);
+		WebElement element = driver.findElement(By.xpath("(//a[@text='Chennai (MAA)'])[2]"));
+		System.out.println("Element found: " + element.getText());
+		element.click();
+
+		
+		
+
+	}
+
+}
---
+++ src/PracticeTraverse.java
@@ -0,0 +1,27 @@
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PracticeTraverse {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Trexie\\Documents\\Installers\\Work\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		//maximize browser size
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		
+		//Sibling - child to parent traverse syntax
+		System.out.println(driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).getText());
+		driver.findElement(By.xpath("//header/div/button[1]/following-sibling::button[1]")).click();
+		//or
+		//child to parent syntaxt
+		driver.findElement(By.xpath("//header/div/button[1]/parent::div/button[2]")).click();
+		
+	}
+
+}
